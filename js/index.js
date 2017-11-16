3e674c3036749d3c34f831db0760c122f7b76317
yuchenLaoNi <32958031+yuchenLaoNi@users.noreply.github.com>
2017-11-16 21:42:42 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -183,3 +183,74 @@ var flag = true; //控制 当点击楼层号时，禁止滚动条的代码执行
 	            .removeClass("active");
 	    }
 	})
+//下面的左右按键（轮播）  通用型!!!!
+$(".next").click(function(){
+	$(".prev").css("cursor","pointer");
+	$mL = parseInt($(".lunbo_con").css("margin-left"));
+	$width = $(".lunbo_con").width();
+	
+//	console.log($width+$mL)
+	if($width+$mL>=2436){
+		$(".lunbo_con").animate({marginLeft:$mL-1108},1000)
+//		console.log(1)
+	}
+	else if($width+$mL<2216 && $width+$mL>1218){
+		$(".lunbo_con").animate({marginLeft:-$width+1218},1000)
+//		console.log($width+$mL)
+	}
+	else{
+		$(".next").css("cursor","not-allowed");
+	}
+})
+//prev
+$(".prev").click(function(){
+	$(".next").css("cursor","pointer");
+	$mL = parseInt($(".lunbo_con").css("margin-left"));
+	$width = $(".lunbo_con").width();
+//	console.log($width+$mL)
+	
+	if($mL == 0){
+		$(".prev").css("cursor","not-allowed");
+		
+	}
+	else if($width+$mL>=1218 && $width+$mL<=2216){
+		;$(".lunbo_con").animate({marginLeft:$mL+1108},1000)
+//		console.log(2)
+	}
+	else{
+		$(".lunbo_con").animate({marginLeft:0},1000)
+//		console.log(1)
+	}
+})
+/*  //非通用！！
+    $(".next").click(function(){
+	$(".prev").css("cursor","pointer");
+	console.log($(".lunbo_con").css("margin-left"))
+	if($(".lunbo_con").css("margin-left") == 0+"px"){
+		$(".next").css("cursor","pointer");
+		$(".lunbo_con").animate({marginLeft:-1200},1000);
+	}
+	else if($(".lunbo_con").css("margin-left") == -1200+"px"){
+		$(".next").css("cursor","pointer");
+		$(".lunbo_con").animate({marginLeft:-2082},1000);
+	}
+	else if($(".lunbo_con").css("margin-left") == -2082+"px"){
+		$(".next").css("cursor","not-allowed");
+	}
+})
+$(".prev").click(function(){
+	$(".next").css("cursor","pointer");
+	if($(".lunbo_con").css("margin-left") == 0+"px"){
+		$(".prev").css("cursor","not-allowed");
+	}
+	else if($(".lunbo_con").css("margin-left") == -1200+"px"){
+		$(".prev").css("cursor","pointer");
+		
+		$(".lunbo_con").animate({marginLeft:0},1000);
+	}
+	else if($(".lunbo_con").css("margin-left") == -2082+"px"){
+		$(".prev").css("cursor","pointer");
+		$(".lunbo_con").animate({marginLeft:-1200},1000);
+	}
+	
+})*/
